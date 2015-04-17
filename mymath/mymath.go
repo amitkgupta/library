9ebbbd9eb6d2017ec5b0d396c05587841a930660
Amit Kumar Gupta <amitkgupta84@gmail.com>
2015-04-17 01:39:57 -0700

    Make test pass, calculate π using Leibniz formula

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -1,5 +1,11 @@
 package mymath
 
+import "math"
+
 func Pi() float64 {
-    return 3.14
+    f := 0.0
+    for k := 0; k <= 1000; k++ {
+        f += 4 * math.Pow(-1, float64(k)) / (2*float64(k) + 1)
+    }
+    return f
 }
